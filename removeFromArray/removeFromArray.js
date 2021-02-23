f429ca87386f8f8f9e55a0083319048229f0c5bd
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-23 17:08:58 +0100

    Fix removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+  let filterArr = [...arr];
+  nums.forEach(function (num) {
+    if (filterArr.includes(num)) {
+      idx = filterArr.indexOf(num);
+      filterArr.splice(idx, 1)
+    }
+  })
 
+  return filterArr
 }
 
 module.exports = removeFromArray
