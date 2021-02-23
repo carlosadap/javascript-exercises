67422ff9a707a930caa386117216b9012105219b
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-23 17:29:24 +0100

    Fix sumAll function

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,40 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (badNums(num1, num2)) {
+    return 'ERROR'
+  }  
 
+  sum = 0
+  
+  if (num1 <= num2) {
+    start = num1  
+    end = num2
+  } else {
+    start = num2
+    end = num1
+  }
+
+  for (start; start <= end; start++) {
+    sum += start
+  }
+
+  return sum
+}
+
+const badNums = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return true
+  }
+
+  if (isNotNumber(num1) || isNotNumber(num2)) {
+    return true
+  }
+
+  return false
+}
+
+const isNotNumber = function isNumber(value) {
+  return typeof value !== 'number' ||
+    !isFinite(value);
 }
 
 module.exports = sumAll
