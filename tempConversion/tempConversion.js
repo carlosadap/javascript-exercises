13a438b89c3e4a6e077b66f6ea92eeb80806cdc9
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-23 21:36:38 +0100

    Fix tempConversino function

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahr) {
+  tempCel = (tempFahr - 32) / 1.8
+  tempCelRound = Math.round(tempCel * 10) / 10
+  return tempCelRound
 }
 
-const ctof = function() {
-
+const ctof = function(tempCel) {
+  tempFahr = tempCel * 1.8 + 32
+  tempFahrRound = Math.round(tempFahr * 10) / 10
+  return tempFahrRound
 }
 
 module.exports = {
