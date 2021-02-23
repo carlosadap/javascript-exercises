fab96b953474e5428858debab0b11efd794f5fa0
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-23 13:28:54 +0100

    Fix reverse function

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  arrStr = str.split("")
+  arrSize = arrStr.length
+  newArr = []
+  while (arrSize >= 0) {
+    newArr.push(arrStr.pop())
+    arrSize--
+  }
 
+  return newArr.join("")
 }
 
 module.exports = reverseString
