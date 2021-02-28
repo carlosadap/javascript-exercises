eeeee61a54ea2d878a83b2d1248ba86d7d55a87b
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-28 19:35:32 +0100

    Add fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let newNum = parseInt(num);
+  if (newNum === 1 || newNum === 2) {
+    return 1
+  } else if (parseInt(newNum) < 1) {
+    return "OOPS"
+  }
 
+  return fibonacci(newNum - 1) + fibonacci(newNum - 2)
 }
 
 module.exports = fibonacci
