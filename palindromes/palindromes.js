57b9482eec60fe8831a25a7cae1f6323fbf7b73b
Carlos Pinheiro <carlosadap@gmail.com>
2021-02-28 14:22:41 +0100

    Add palindrome check

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  const array = str.split("");
+  const lettersArray = array.filter(char => isLetter(char));
+  const lettersArrayReversed = reverseArr(lettersArray);
+  return arrayToString(lettersArray) === arrayToString(lettersArrayReversed);
 }
+
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z]/i);
+}
+
+function reverseArr(array) {
+  const reversedArr = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArr.push(array[i]);
+  }
+  return reversedArr;
+}
+
+function arrayToString(array) {
+  return array.join("").toLowerCase()
+}
+
+palindromes('ZZZZ car, a man, a maraca.')
 
 module.exports = palindromes
