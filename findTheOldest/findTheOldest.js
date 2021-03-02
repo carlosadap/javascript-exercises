10b9d4fdd3aafaae4e93a7b6d689db7e0e8cd3c9
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-02 11:13:37 +0100

    Add find the oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-let findTheOldest = function() {
+let findTheOldest = function(array) {
+  const oldestPerson = array.reduce((firstPerson, currentPerson) => {
+    return older(firstPerson, currentPerson);
+  })
 
+  return oldestPerson
+}
+
+function older(person1, person2) {
+  if (currentAge(person1) > currentAge(person2)) {
+    return person1;
+  } else {
+    return person2;
+  }
+}
+
+function currentAge(person) {
+  const currentDate = new Date();
+  const cYear = currentDate.getFullYear();
+  let agePerson;
+  if ("yearOfDeath" in person) {
+    agePerson = person.yearOfDeath - person.yearOfBirth;
+  } else {
+    agePerson = cYear - person.yearOfBirth;
+  }
+  return agePerson
 }
 
 module.exports = findTheOldest
